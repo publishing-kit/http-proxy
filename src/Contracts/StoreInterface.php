b3736dd01959a84803ab9fcffb44014a9ca5dd4f
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-12-08 20:09:08 +0000

    Copied over store interface

---
+++ src/Contracts/StoreInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PublishingKit\HttpProxy\Contracts;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+interface StoreInterface
+{
+    public function get(ServerRequestInterface $request): ?string;
+
+    public function put(ServerRequestInterface $request, ResponseInterface $response): void;
+}
