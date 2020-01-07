e20ca39a0b6912a311af60a12747a5a7828927a9
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-01-07 20:10:29 +0000

    Rewrote proxy class

--- src/Proxy.php
+++ src/Proxy.php
@@ -4,29 +4,46 @@ declare(strict_types=1);
 
 namespace PublishingKit\HttpProxy;
 
-use PublishingKit\HttpProxy\Contracts\StoreInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Http\Client\HttpClient;
+use Http\Client\Common\PluginClient;
+use Http\Client\Common\Plugin\CachePlugin;
+use Http\Message\StreamFactory;
 
 final class Proxy
 {
     /**
-     * @var StoreInterface
+     * @var HttpClient
      */
-    private $store;
+    private $client;
 
-    public function __construct(StoreInterface $store)
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $cache;
+
+    /**
+     * @var StreamFactory
+     */
+    private $streamFactory;
+
+    public function __construct(HttpClient $client, CacheItemPoolInterface $cache, StreamFactory $streamFactory)
     {
-        $this->store = $store;
+        $this->client = $client;
+        $this->cache = $cache;
+        $this->streamFactory = $streamFactory;
     }
 
-    public function get(RequestInterface $request, callable $callback): ResponseInterface
+    public function handle(RequestInterface $request): ResponseInterface
     {
-        // check if method is cacheable
-        if (!in_array($request->getMethod(), ['GET', 'HEAD'])) {
-            return $callback($request);
-        }
+        $cachePlugin = new CachePlugin($this->cache, $this->streamFactory, []);
 
-        return $callback($request);
+        $pluginClient = new PluginClient(
+            $this->client,
+            [$cachePlugin]
+        );
+        return $pluginClient->sendRequest($request);
     }
 }
