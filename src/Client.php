5c70d48037c450b2e8ec4271bbc3514319ab2866
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-01-07 19:58:11 +0000

    Created client class

---
+++ src/Client.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PublishingKit\HttpProxy;
+
+use Http\Client\HttpClient;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class Client implements HttpClient
+{
+    /**
+     * @var callable
+     */
+    private $callback;
+
+    public function __construct(callable $callback)
+    {
+        $this->callback = $callback;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        $callback = $this->callback;
+        return $callback($request);
+    }
+}
